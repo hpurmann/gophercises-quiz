5d4f943497d063b2621fa7f8325275f1e7139e49
Hendrik Purmann <h.purmann@googlemail.com>
2022-08-01 17:06:10 +0200

    Add basic answer to quiz

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	filename := "problems.csv"
+	fileContent, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: cannot read file ", filename, err)
+	}
+
+	reader := csv.NewReader(strings.NewReader(string(fileContent)))
+	fields, err := reader.ReadAll()
+
+	if err != nil {
+		fmt.Println("Error marshalling csv", err)
+	}
+
+	var numCorrect int
+
+	for index, questionAnswer := range fields {
+		question, actualAnswer := questionAnswer[0], questionAnswer[1]
+		fmt.Printf("Question %d: %s = ", index+1, question)
+
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		answer := scanner.Text()
+
+		if strings.TrimSpace(answer) == actualAnswer {
+			numCorrect += 1
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", numCorrect, len(fields))
+}
